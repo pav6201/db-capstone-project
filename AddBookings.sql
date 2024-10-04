33d7b19139276e0dac2b1da03b8c7de3f63544e7
pav6201 <163483675+pav6201@users.noreply.github.com>
2024-10-04 19:27:02 -0400

    Update AddBookings.sql

--- AddBookings.sql
+++ AddBookings.sql
@@ -1,4 +1,4 @@
-CREATE DEFINER=`metauser`@`%` PROCEDURE `AddBooking`(p_BookingID INT, p_CustomerID INT,p_TableNumber INT, p_BookingDate DATE )
+CREATE DEFINER=`MUser`@`%` PROCEDURE `AddBooking`(p_BookingID INT, p_CustomerID INT,p_TableNumber INT, p_BookingDate DATE )
 BEGIN
 
 INSERT INTO Bookings(BookingID,CustID,TableNumber,BookingDate) VALUES (p_BookingID, p_CustomerID, p_TableNumber, DATE(p_BookingDate));
