7302dcd36eed3569b2e4383c6bb388a985cba518
pav6201 <163483675+pav6201@users.noreply.github.com>
2024-10-04 19:25:19 -0400

    Add files via upload

---
+++ AddBookings.sql
@@ -0,0 +1,6 @@
+CREATE DEFINER=`metauser`@`%` PROCEDURE `AddBooking`(p_BookingID INT, p_CustomerID INT,p_TableNumber INT, p_BookingDate DATE )
+BEGIN
+
+INSERT INTO Bookings(BookingID,CustID,TableNumber,BookingDate) VALUES (p_BookingID, p_CustomerID, p_TableNumber, DATE(p_BookingDate));
+SELECT CONCAT('New booking added') AS 'Confirmation';
+END
