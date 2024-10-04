baab6c6b3241806cf89c3fdced1f2752d3e10ca9
pav6201 <163483675+pav6201@users.noreply.github.com>
2024-10-04 19:37:02 -0400

    Update and rename UpdateBookingSP.sql to UpdateBooking.sql

--- UpdateBooking.sql
+++ UpdateBooking.sql
@@ -1,4 +1,4 @@
-CREATE DEFINER=`Muser`@`%` PROCEDURE `UpdateBooking`(p_BookingID int,p_BookingDate DATE)
+CREATE DEFINER=`user`@`%` PROCEDURE `UpdateBooking`(p_BookingID int,p_BookingDate DATE)
 BEGIN
 UPDATE Bookings SET BookingDate = (DATE(p_BookingDate)) WHERE BookingID = p_BookingID ;
 SELECT CONCAT('Booking ',p_BookingID,' updated') AS 'Confirmation';
