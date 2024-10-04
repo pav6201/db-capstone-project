06e87d442bca27056a3ca813e1d1b8c9663a0a1f
pav6201 <163483675+pav6201@users.noreply.github.com>
2024-10-04 19:37:24 -0400

    Update CancelBooking.sql

--- CancelBooking.sql
+++ CancelBooking.sql
@@ -1,4 +1,4 @@
-CREATE DEFINER=`Muser`@`%` PROCEDURE `CancelBooking`(p_BookingID INT)
+CREATE DEFINER=`user`@`%` PROCEDURE `CancelBooking`(p_BookingID INT)
 BEGIN
 DELETE FROM Bookings WHERE BookingID = p_BookingID;
 SELECT CONCAT('Booking ', p_BookingID, ' cancelled') AS 'Confirmation';
